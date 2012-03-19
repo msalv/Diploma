4090f4e47f8805774381dcd03122691a2b8fcd22
Main Salvadore <bikovgta13@gmail.com>
2012-03-19 15:22:53 +0400

    Added sample local_settings.php file

---
+++ local_settings.sample.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * Sample local settings
+ * Individual for every project.
+ * Edit and rename it to local_settings.php
+ */
+
+// DB connection settings
+const DB_ENGINE = 'mysql';
+const DB_HOST = 'localhost';
+const DB_NAME = 'diploma';
+
+const DB_USER = 'root';
+const DB_PASS = 'root';
+
+define('CONN_STRING', DB_ENGINE . ':host='. DB_HOST . ';dbname=' . DB_NAME);
+
+/**
+ * PDO_ERRMODE
+ * Options:
+ *     PDO::ERRMODE_SILENT
+ *     PDO::ERRMODE_WARNING
+ *     PDO::ERRMODE_EXCEPTION
+ */
+const PDO_ERRMODE = PDO::ERRMODE_SILENT;
+
+?>
