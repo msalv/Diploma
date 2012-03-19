e1ca603b5ac01dbee6112f211c1eb2af846b2297
Main Salvadore <bikovgta13@gmail.com>
2012-03-19 14:51:37 +0400

    Implemented default behavior for fetching (formal 'find' method) including select(), where(), limit() and orderBy() methods

--- core/Mapper.php
+++ core/Mapper.php
@@ -7,10 +7,78 @@
  * @author Kirill
  */
 abstract class Mapper {
+    
+    /**
+     * @property DBO $_DBH Instance of DBO class
+     */
+    protected $_DBH;
+    protected $_select = "*";
+    protected $_where = "";
+    protected $_limit = "";
+    protected $_order = "";
+    protected $_params = array();
+    
+    public function __construct() {
+        
+        try {
+            $this->_DBH = new PDO(CONN_STRING, DB_USER, DB_PASS);
+            
+            $this->_DBH->setAttribute(PDO::ATTR_ERRMODE, PDO_ERRMODE);
+        }
+        catch (PDOException $e) {
+            echo "<pre>" . $e->getMessage() . "</pre>"; // just for now
+        }
+    }
+    
+    public function __destruct() {
+        $this->_DBH = null;
+    }
+
     public abstract function save($modelObject);
-    public abstract function find($options);
+    public abstract function fetch();
     public abstract function update($modelObject);
     public abstract function delete($modelObject);
+    
+    public function select($fields) {
+        
+        $fields = ( !is_array($fields) ) ? func_get_args() : $fields;
+        
+        $this->_select = implode(', ',  $fields);
+        
+        return $this;
+    }
+    
+    public function where($search) {
+        
+        $where = array_map(
+            function ($key) {
+                return $key .= "=:$key";
+            }, 
+            array_keys($search)
+        );
+            
+        $this->_where = " WHERE " . implode(' AND ', $where);
+        $this->_params = $search;
+            
+        return $this;
+    }
+    
+    public function limit($start, $duration) {
+        
+        if ( is_numeric($start) && is_numeric($duration) ) {
+            
+            $start = (int)$start;
+            $duration = (int)$duration;
+            
+            $this->_limit = " LIMIT $start, $duration";
+        }
+        return $this;
+    }
+    
+    public function orderBy() {
+        $this->_order = " ORDER BY " . implode(', ', func_get_args() );
+        return $this;
+    }
 }
 
 ?>
