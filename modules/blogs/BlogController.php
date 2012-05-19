26f69099eebb45b8bf662b37303e2c75981aa63f
Main Salvadore <bikovgta13@gmail.com>
2012-05-19 18:27:36 +0400

    Implementing settings, posting, getting and subscribing for blogs

--- modules/blogs/BlogController.php
+++ modules/blogs/BlogController.php
@@ -1,6 +1,12 @@
 <?php
 
+if ( !defined('BLOGS_ROOT') ) {
+    define('BLOGS_ROOT', PROJECT_ROOT . '/modules/blogs');
+}
+
 require_once PROJECT_ROOT . '/core/Controller.php';
+require_once PROJECT_ROOT . '/core/XMLViewLoader.php';
+require_once BLOGS_ROOT . '/BlogMapper.php';
 
 /**
  * Description of BlogController
@@ -8,5 +14,173 @@ require_once PROJECT_ROOT . '/core/Controller.php';
  * @author Kirill
  */
 class BlogController extends Controller {
-    //put your code here
+    
+    public function __construct( $view ) {
+        $this->_mapper = new BlogMapper();
+        $this->_view = new XMLViewLoader($view);
+    }
+    
+    /**
+     * Getting blog and it's posts
+     * @param mixed $id Blog id or user's login
+     * @param boolean $personal True if blog is personal
+     * @param integer $start Offset
+     * @return array Array of Blog 
+     */
+    public function getBlog($id, $personal = false, $start = 0) {
+        
+        if ($personal) {
+            return $this->_mapper->getWallPosts($id, $start * 10);
+        }
+        else {
+            return $this->_mapper->getBlogPosts($id, $start * 10);
+        }
+        
+    }
+    
+    /**
+     * Postring content to the user's wall
+     * @param string $user User's login
+     */
+    public function postToWall($user) {
+        $this->_checkPostData('content', 'title', 'type');
+        
+        if ( !empty($_POST['content']) ) {
+            
+            $this->_mapper->insertPost($user, $_POST['content']);
+        }
+    }
+    
+    /**
+     * Getting list of blogs from the $start
+     * @param integer $start Offset
+     * @return array Array of Blog 
+     */
+    public function getBlogList($start = 0) {
+        
+        $fields = array('id', 'title', 'info', 'type');
+        
+        $this->_mapper->
+                select($fields)
+                ->where( array( 'type' => '2' ) )
+                ->orderBy('id')
+                ->limit($start * 10, 30);
+        
+        return $this->_mapper->fetch();
+    }
+    
+    /**
+     * Performs sone action on the blog page
+     * @param type $id 
+     */
+    public function performAction($id) {
+        
+        $this->_checkPostData('action', 'content', 'title', 'type');
+        
+        switch ($_POST['action']) {
+            
+            case 'subscribe':
+                $this->_mapper->subscribe($id, $_SESSION['id']);
+                break;
+            case 'unsubscribe':
+                $this->_mapper->unsubscribe($id, $_SESSION['id']);
+                break;
+            default:
+                $this->_checkPostData('content', 'title', 'type');
+                              
+                // validation
+                if ( empty($_POST['title']) ) {
+                    die('Input title');
+                }
+                else if ( empty($_POST['content']) ) {
+                    die('Input message');
+                }
+                else if ( empty($_POST['type']) ) {
+                    $_POST['type'] = '1';
+                }
+                
+                $this->_mapper->insertPost($id, $_POST['content'], $_POST['title'], $_POST['type']);
+
+                break;
+        }
+        
+    }
+    
+    /**
+     * Gets subscribers of the blog
+     * @param integer $blog_id Blog id
+     * @param integer $start Offset
+     * @param integer $amount Amount of people to get
+     * @return array Array of Person 
+     */
+    public function getSubscribers($blog_id, $start = 0, $amount = 30) {
+        return $this->_mapper->fetchSubscribers($blog_id, $start * $amount, $amount);
+    }
+    
+    /**
+     * Gets blog settings
+     * @param integer $blog_id Blog id
+     * @return array Array of Blog on success 
+     */
+    public function getSettings($blog_id) {
+        
+        if ( !$this->_mapper->isOwner( $blog_id, $_SESSION['id'] ) ) {
+            die('Access denied');
+        }
+        
+        $fields = array('id', 'title', 'info', 'type', 'locked');
+        
+        $search = array( 'id' => $blog_id );
+        
+        $this->_mapper->select($fields)->where($search);
+        
+        return $this->_mapper->fetch();
+    }
+    
+    /**
+     * Updates blog settrings 
+     * @param integer $blog_id Blog id
+     */
+    public function updateSettings($blog_id) {
+        
+        if ( !$this->_mapper->isOwner( $blog_id, $_SESSION['id'] ) ) {
+            die('Access denied');
+        }
+        
+        $this->_checkPostData('title', 'info', 'type', 'locked');
+               
+        $dom = new DOMDocument('1.0', 'utf-8');
+        $dom->appendChild( new DOMElement('Blog') );
+        
+        $xml = simplexml_import_dom($dom);
+        $xml->addChild('meta');
+                
+        $xml->addAttribute('id', $blog_id);
+        
+        if ( empty($_POST['title']) ) {
+            $xml->meta[0]->addChild('message', 'Вы не указали название группы')->addAttribute('type', 'error');
+        }
+        
+        $type = intval($_POST['type']);
+        
+        $xml->addChild('title', $_POST['title']);
+        $xml->addChild('info', $_POST['info']);
+        $xml->addAttribute('type',  ($type < 2) ? 2 : $type );
+        $xml->addAttribute('locked', $_POST['locked'] == '1' ? '1' : '0');
+           
+        if ( !$xml->meta[0]->count() ) {
+            try {
+                $this->_mapper->set((array)$xml)->where( array( 'id' => $blog_id ) );
+                $this->_mapper->update();
+                $xml->meta[0]->addChild('message', 'Настройки группы обновлены')->addAttribute('type', 'success');
+            }
+            catch (PDOException $e) {
+                $xml->meta[0]->addChild('message', 'При обращении к базе данных произошла ошибка')->addAttribute('type', 'error');
+            }
+        }
+        
+        // load view anyway
+        $this->_view->load($dom);
+    }
+    
 }
--- modules/blogs/BlogMapper.php
+++ modules/blogs/BlogMapper.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once PROJECT_ROOT . '/core/Mapper.php';
+require_once BLOGS_ROOT . '/Blog.php';
 
 /**
  * Description of BlogMapper
@@ -15,39 +16,304 @@ class BlogMapper extends Mapper {
     
     public function fetch() {
               
-        /*$sql = "SELECT $this->_select FROM people"
+        $sql = "SELECT $this->_select FROM blogs"
                 . $this->_where
                 . $this->_order
                 . $this->_limit;
               
         // Prepare and execute statement fetching to Person class
         $STH = $this->_DBH->prepare($sql);
-        $STH->setFetchMode(PDO::FETCH_CLASS, 'Person');
+        $STH->setFetchMode(PDO::FETCH_CLASS, 'Blog');
         
         $STH->execute($this->_params);
         
         $this->clear();
         
         return $STH->fetchAll();
-         * 
-         */
     }
     
     public function update() {
         
-        /*$sql = "UPDATE people" . $this->_set . $this->_where;
+        $sql = "UPDATE blogs" . $this->_set . $this->_where;
         
         $STH = $this->_DBH->prepare($sql);
         
         $STH->execute($this->_params);
         
         $this->clear();
-         * 
-         */
     }
     
     public function delete($modelObject) {
         
     }
     
+    public function subscribe($id, $user_id) {
+        $STH = $this->_DBH->prepare("INSERT INTO subscribers_blog VALUES (:user_id, :id)");
+        try {
+            $STH->execute( array('id' => $id, 'user_id' => $user_id) );
+        }
+        catch (PDOException $e) {
+            return "error";
+        }
+    }
+    
+    public function unsubscribe($id, $user_id) {
+        $STH = $this->_DBH->prepare("DELETE FROM subscribers_blog 
+            WHERE subscriber_id=:user_id AND blog_id=:id");
+        $STH->execute( array('id' => $id, 'user_id' => $user_id) );
+    }
+    
+    public function insertPost($id, $content, $title = null, $type = '1') {
+        
+        // if blog's $id is not a number then try get it as personal blog's owner login
+        if ( !is_numeric($id) ) {
+            $sql = "SELECT blogs.id, blogs.locked, blogs.type 
+                    FROM blogs, people, owners_blog 
+                    WHERE people.login=:blog 
+                        AND people.id=owners_blog.owner_id 
+                        AND owners_blog.blog_id=blogs.id 
+                        AND blogs.type='1'";
+        }
+        else {
+            $sql = "SELECT blogs.id, blogs.locked, blogs.type 
+                    FROM blogs 
+                    WHERE blogs.id=:blog";
+        }
+        
+        $STH = $this->_DBH->prepare($sql);
+        $STH->execute( array('blog' => $id) );
+        
+        $blog = $STH->fetch(PDO::FETCH_ASSOC);
+        
+        if ( empty($blog) ) {
+            header("HTTP/1.0 404 Not Found");
+            die("Blog not found");
+        }
+        
+        // checking for user as a subscriber of the blog
+        $can_post = true;
+        
+        if ($blog['locked'] == '1') {
+            $can_post = $this->_isSubscriber($blog['id'], $_SESSION['id']);
+        }
+        
+        if ( $can_post ) {
+            $blog['locked'] = '0';
+            
+            $sql = "INSERT INTO 
+                posts (blog_id, author_id, title, content, pub_date, post_type) 
+                VALUES (:blog_id, :poster_id, :title, :content, NOW(), :type);";
+                            
+            $STH = $this->_DBH->prepare($sql);
+
+            $STH->execute( array(
+                'blog_id' => $blog['id'],
+                'poster_id' => $_SESSION['id'],
+                'title' => $title,
+                'content' => $content,
+                'type' => $type
+            ) );
+            
+        }
+    }
+    
+    /**
+     * Checks whether user is subscriber of blog or not
+     * @param integer $blog_id Blog id
+     * @param integer $user_id User id
+     * @return boolean True on success or False on failure
+     */
+    private function _isSubscriber($blog_id, $user_id) {
+       
+        $STH = $this->_DBH
+                ->prepare("SELECT 1 as can_post FROM subscribers_blog 
+                    WHERE subscriber_id=:user_id AND blog_id=:blog_id");
+
+        $STH->execute( array('blog_id' => $blog_id, 'user_id' => $user_id) );
+        
+        $subscription = $STH->fetch(PDO::FETCH_ASSOC);
+        
+        return !empty( $subscription );
+    }
+    
+    /**
+     * Checks whether user is owner of the blog or not
+     * @param integer $blog_id Blog id
+     * @param integer $user_id User id
+     * @return boolean True on success or False on failure
+     */
+    public function isOwner($blog_id, $user_id) {
+        
+        $STH = $this->_DBH
+                ->prepare("SELECT 1 AS result FROM blogs, owners_blog 
+                    WHERE id=:blog_id AND owner_id=:user_id AND blog_id=id");
+        
+        $STH->execute( array('blog_id' => $blog_id, 'user_id' => $user_id) );
+        
+        $ownership = $STH->fetch(PDO::FETCH_ASSOC);
+        
+        return !empty( $ownership );
+    }
+    
+    public function getWallPosts($login, $start) {
+              
+        // getting wall info
+        $STH = $this->_DBH
+                ->prepare("SELECT blogs.id, blogs.locked, 
+                    people.id AS user_id, people.first_name, people.login, people.gender, 
+                    people.last_name, people.middle_name, people.picture_url 
+                    FROM blogs, people, owners_blog 
+                    WHERE people.login=:login 
+                        AND people.id=owners_blog.owner_id 
+                        AND owners_blog.blog_id=blogs.id 
+                        AND blogs.type='1'");
+        
+        $STH->execute( array('login' => $login) );
+        
+        $wall = $STH->fetch(PDO::FETCH_ASSOC);
+        
+        if ( empty($wall) ) {
+            header("HTTP/1.0 404 Not Found");
+            die("Wall not found");
+        }
+
+        // checking for user as a subscriber of the blog
+
+        if ( $this->_isSubscriber($wall['id'], $_SESSION['id']) ) {
+            $wall['is_friend'] = true;
+            $wall['locked'] = '0';
+        }
+        else {
+            $wall['is_friend'] = false;
+        }
+        
+        // if user a subscriber then get posts from the blog
+        $posts = array();
+        
+        if ( $wall['locked'] == '0' ) {
+            
+            $sql = "SELECT posts.id, posts.title, posts.content, posts.pub_date, posts.enabled, 
+                people.first_name, people.last_name, people.picture_url, 
+                people.id AS author_id, people.login, people.gender, posts.comm_num 
+                FROM posts, people 
+                WHERE blog_id=:blog_id AND author_id=people.id 
+                ORDER BY posts.id DESC 
+                LIMIT $start, 10";
+            
+            $STH = $this->_DBH->prepare($sql);
+            
+            $STH->execute( array('blog_id' => $wall['id']) );
+            
+            $posts = $STH->fetchAll(PDO::FETCH_ASSOC);
+        }
+               
+        $owner = array(
+            'id' => $wall['user_id'],
+            'first_name' => $wall['first_name'],
+            'middle_name' => $wall['middle_name'],
+            'last_name' => $wall['last_name'],
+            'picture_url' => $wall['picture_url'],
+            'login' => $wall['login'],
+            'gender' => $wall['gender'],
+            'is_friend' => $wall['is_friend']
+        );
+        
+        $blog = array(
+            'id' => $wall['id'],
+            'locked' => $wall['locked'],
+            'type' => 1,
+            'owners' => array($owner),
+            'posts' => $posts,
+            'title' => null,
+            'info' => null,
+            'subscribed' => $wall['is_friend']
+        );
+        
+        return array( new Blog($blog) );
+    }
+    
+    public function getBlogPosts($blog_id, $start) {
+              
+        // getting blog info
+        $STH = $this->_DBH
+                ->prepare("SELECT id, title, info, type, locked 
+                    FROM blogs WHERE id=:blog_id AND type<>'1'");
+        
+        $STH->execute( array('blog_id' => $blog_id) );
+        
+        $blog = $STH->fetch(PDO::FETCH_ASSOC);
+        
+        if ( empty($blog) ) {
+            header("HTTP/1.0 404 Not Found");
+            die("Blog not found");
+        }
+       
+        if ( $this->_isSubscriber($blog_id, $_SESSION['id']) ) {
+            $blog['subscribed'] = true;
+            $blog['locked'] = '0';
+        }
+        else {
+            $blog['subscribed'] = false;
+        }
+        
+        // if user a subscriber then get posts from the blog
+        $posts = array();
+        
+        if ( $blog['locked'] == '0' ) {
+           
+            $sql = "SELECT posts.id, posts.title, posts.content, posts.pub_date, posts.enabled, 
+                people.first_name, people.last_name, people.gender, people.picture_url, 
+                people.id AS author_id, people.login, posts.comm_num 
+                FROM posts, people 
+                WHERE blog_id=:blog_id AND author_id=people.id 
+                ORDER BY posts.id DESC 
+                LIMIT $start, 10";
+            
+            $STH = $this->_DBH->prepare($sql);
+            
+            $STH->execute( array('blog_id' => $blog_id) );
+            
+            $posts = $STH->fetchAll(PDO::FETCH_ASSOC);
+        }
+                     
+        $blog['posts'] = $posts;
+        
+        // getting owners
+        $STH = $this->_DBH->prepare("SELECT id, login, first_name, middle_name, 
+            last_name, picture_url, gender, 1 AS is_friend 
+            FROM people, owners_blog WHERE people.id=owners_blog.owner_id 
+            AND owners_blog.blog_id=:blog_id");
+        
+        $STH->execute( array('blog_id' => $blog_id) );
+        
+        $owners = $STH->fetchAll(PDO::FETCH_ASSOC);
+        
+        $blog['owners'] = $owners;
+        
+        return array( new Blog($blog) );
+    }
+    
+    /**
+     * Fetch $amount subscribers of the $blog_id from $start
+     * @param integer $blog_id Blog Id
+     * @param integer $start Fetching offset
+     * @param integer $amount Amount of people to fetch
+     * @return array Array of Person
+     */
+    public function fetchSubscribers($blog_id, $start, $amount) {
+        
+        $STH = $this->_DBH->prepare(
+                "SELECT id, login, first_name, middle_name, 
+                    last_name, picture_url, gender, location 
+                FROM people, subscribers_blog AS sb 
+                WHERE people.id=sb.subscriber_id 
+                AND sb.blog_id=:blog_id 
+                LIMIT $start, $amount");
+        
+        $STH->setFetchMode(PDO::FETCH_CLASS, 'Person');
+        $STH->execute( array('blog_id' => $blog_id) );
+        
+        return $STH->fetchAll();
+    }
+
 }
