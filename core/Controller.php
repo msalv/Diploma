064f552b26833d136b00c0ba97fbb94746826d31
Main Salvadore <bikovgta13@gmail.com>
2012-05-19 15:57:23 +0400

    Moved _checkPostData() and loadView() methods to base class

--- core/Controller.php
+++ core/Controller.php
@@ -7,4 +7,33 @@
 abstract class Controller {
     protected $_mapper;
     protected $_view;
+    
+    /**
+     * Checks if $_POST variable contains all required data. 
+     * If data not set the function sets them to empty string. 
+     */
+    protected function _checkPostData() {
+        $keys = func_get_args();
+        
+        foreach ($keys as $key) {
+            if ( !array_key_exists($key, $_POST) ) {
+                $_POST[$key] = '';
+            }
+            else {
+                $_POST[$key] = trim( $_POST[$key] );
+            }
+        }
+    }
+    
+    public function loadView($modelData = null, $rootName = null, $makeSet = false) {
+        if ( !empty($modelData) ) {
+            $modelData = $this->_view->prepare($modelData, $makeSet);
+        }
+        
+        //header('Content-type: text/xml');
+        //echo $modelData->saveXML();
+        //exit();
+              
+        $this->_view->load($modelData, $rootName);
+    }
 }
