870d7cc8e5d046b5ec8f8a0d7e61a21fc90b9c15
Main Salvadore <bikovgta13@gmail.com>
2012-05-19 18:25:12 +0400

    Added owners and posts array fields, add some logic to constructor

--- modules/blogs/Blog.php
+++ modules/blogs/Blog.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once PROJECT_ROOT . '/core/Model.php';
+require_once PROJECT_ROOT . '/modules/blogs/Post.php';
+require_once PROJECT_ROOT . '/modules/people/Person.php';
 
 /**
  * Description of Blog
@@ -11,8 +13,31 @@ class Blog implements Model {
     private $id;
     private $title;
     private $info;
-    private $blog_type;
+    private $type;
     private $locked;
+    // following not in the table
+    private $owners = array();
+    private $posts = array();
+    private $subscribed;
+    
+    public function __construct($data = null) {
+        if ( !empty($data) ) {
+            $this->id = $data['id'];
+            $this->title = $data['title'];
+            $this->info = $data['info'];
+            $this->type = $data['type'];
+            $this->locked = $data['locked'];
+            $this->subscribed = $data['subscribed'];
+            
+            foreach ($data['owners'] as $p) {
+                array_push($this->owners, new Person($p) );
+            }
+            
+            foreach ($data['posts'] as $t) {
+                array_push($this->posts, new Post($t) );
+            }
+        }
+    }
     
     public function toDOMElement($root) {            
         $node = new DOMElement(__CLASS__);
@@ -23,8 +48,23 @@ class Blog implements Model {
                 switch ($name) {
                     case 'id':
                     case 'locked':
-                    case 'blog_type':
+                    case 'type':
+                    case 'subscribed':
                         $node->setAttribute($name, $value);
+                        break;
+                    
+                    case 'owners':
+                        $owners = $node->appendChild( new DOMElement($name) );
+                        foreach ($value as $p) {
+                            $p->toDOMElement($owners);
+                        }
+                        break;
+                        
+                    case 'posts':
+                        $posts = $node->appendChild( new DOMElement($name) );
+                        foreach ($value as $t) {
+                            $t->toDOMElement($posts);
+                        }
                         break;
 
                     default:
