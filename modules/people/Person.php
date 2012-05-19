1a5ecaf8a2ee643d3b4eb3f0478a38b289472f3f
Main Salvadore <bikovgta13@gmail.com>
2012-05-19 16:05:36 +0400

    Privacy field replaced with three different fields. Implemented constructor behavior. Added friends and blogs arrays as new class fields. Added few setters and getters

--- modules/people/Person.php
+++ modules/people/Person.php
@@ -27,8 +27,28 @@ class Person implements Model {
     private $location;
     private $timezone;
     private $person_type;
-    private $privacy;
+    private $privacy_wall;
+    private $privacy_info;
+    private $privacy_friends;
     private $last_login;
+    
+    //not in the table
+    private $friends;
+    private $is_friend;
+    private $blogs;
+    
+    public function __construct($data = null) {
+        if ( !empty($data) ) {
+            $this->id = $data['id'];
+            $this->first_name = $data['first_name'];
+            $this->last_name = $data['last_name'];
+            $this->middle_name = $data['middle_name'];
+            $this->picture_url = $data['picture_url'];
+            $this->login = $data['login'];
+            $this->gender = $data['gender'];
+            $this->is_friend = $data['is_friend'];
+        }
+    }
     
     /**
      * Converts object to a DOMElement
@@ -45,6 +65,9 @@ class Person implements Model {
                 switch ($name) {
                     case 'id':
                     case 'gender':
+                    case 'privacy_wall':
+                    case 'privacy_info':
+                    case 'privacy_friends':
                         $node->setAttribute($name, $value);
                         break;
                     
@@ -54,7 +77,21 @@ class Person implements Model {
                         $date->setAttribute('month', substr($value, 5, 2));
                         $date->setAttribute('year', substr($value, 0, 4));
                         break;
-
+                    
+                    case 'friends':
+                    case 'blogs':
+                        $list = $node->appendChild( new DOMElement($name) );
+                        foreach ($value as $i) {
+                            $i->toDOMElement($list);
+                        }
+                        break;
+                        
+                    case 'is_friend':
+                        if ($value) {
+                            $node->appendChild( new DOMElement($name, $value) );
+                        }
+                        break;
+                    
                     default:
                         $node->appendChild( new DOMElement($name, $value) );
                         break;
@@ -84,6 +121,46 @@ class Person implements Model {
     
     public function getType() {
         return $this->person_type;
+    }
+    
+    public function getTimeZone() {
+        return $this->timezone;
+    }
+    
+    public function getPrivacyWall() {
+        return $this->privacy_wall;
+    }
+    
+    public function setPrivacyWall($val) {
+        $this->privacy_wall = $val;
+    }
+    
+    public function getPrivacyInfo() {
+        return $this->privacy_info;
+    }
+    
+    public function setPrivacyInfo($val) {
+        $this->privacy_info = $val;
+    }
+    
+    public function getPrivacyFriends() {
+        return $this->privacy_friends;
+    }
+    
+    public function setPrivacyFriends($val) {
+        $this->privacy_friends = $val;
+    }
+    
+    public function setFriends($friends) {
+        $this->friends = $friends;
+    }
+    
+    public function setIsFriend($val) {
+        $this->is_friend = $val;
+    }
+    
+    public function setBlogs($blogs) {
+        $this->blogs = $blogs;
     }
 }
 
