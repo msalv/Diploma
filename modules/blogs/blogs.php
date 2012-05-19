8a868ee5aa3e7251c61660ae199e792c41eb5249
Main Salvadore <bikovgta13@gmail.com>
2012-05-19 18:24:04 +0400

    Added rerouting for blogs module

--- modules/blogs/blogs.php
+++ modules/blogs/blogs.php
@@ -1,11 +1,66 @@
 <?php
 
 // Define current module root directory
-define( 'MODULE_ROOT', dirname(__FILE__) );
+define( 'BLOGS_ROOT', dirname(__FILE__) );
 
-require MODULE_ROOT . '/Blog.php';
-require MODULE_ROOT . '/BlogMapper.php';
-require MODULE_ROOT . '/BlogController.php';
-require PROJECT_ROOT . '/core/XSLViewLoader.php';
+require_once BLOGS_ROOT . '/BlogController.php';
 
-?>
+if ( isset($subs) ) {
+    $c = new BlogController('blogs/subs.xsl');
+      
+    if ( isset($page) ) {
+        $people = $c->getSubscribers($blog_id, $page);
+    }
+    else if ( isset($_GET['mode']) ) {
+        if ($_GET['mode'] == 'block') {
+            $people = $c->getSubscribers($blog_id, 0, 6);
+        }
+        else {
+            $people = $c->getSubscribers($blog_id);
+        }
+    }
+    else {
+        $people = $c->getSubscribers($blog_id);
+    }
+
+    $c->loadView($people, null, true);
+}
+// settings
+else if ( isset($settings) ) {
+    $c = new BlogController('blogs/settings.xsl');
+
+    if ( !empty($_POST) ) {
+        
+        $c->updateSettings($blog_id);
+        exit();
+    }
+    
+    $blog = $c->getSettings($blog_id);
+
+    $c->loadView($blog);
+}
+// show blog
+else if ( isset($blog_id) ) {
+    $c = new BlogController('blogs/casual.xsl');
+
+    if ( !empty($_POST) ) {
+        
+        $c->performAction($blog_id);
+    }
+    
+    $blog = $c->getBlog($blog_id);
+
+    $c->loadView($blog);
+}
+else {
+    $c = new BlogController('blogs/blogs.xsl');
+    
+    if (isset($page) ) {
+        $list = $c->getBlogList($page);
+        $c->loadView($list, null, true);
+    }
+    else {
+        $list = $c->getBlogList();
+        $c->loadView($list);
+    }
+}
